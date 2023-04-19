664e4a2f7324bc52d6469fd79478623e7edbc5d6
gracy94 <gracielafaria94@gmail.com>
2023-04-19 11:38:09 -0300

    Image and Form added to app

--- cryptocurrency-quote/src/App.jsx
+++ cryptocurrency-quote/src/App.jsx
@@ -1,10 +1,63 @@
 import { useState } from 'react'
+import styled from '@emotion/styled'
+import Form from './components/Form'
+import CryptoImage from './img/cryptocurrency.png'
 
 function App() {
 
   return (
-   <h1>From App</h1>
+    <Container>
+      <Image
+        src={CryptoImage}
+        alt='Cryptocurrency Image'
+      />
+      <div>
+        <Heading>Calculate Cryptocurrencies Instantly</Heading>
+        <Form></Form>
+      </div>
+      
+    </Container>
+   
   )
 }
 
 export default App
+
+//Emotion Styled components
+
+const Heading = styled.h1`
+  font-family: 'Merriweather', serif;
+  color: #FFF;
+  text-align: center;
+  font-weight: 700;
+  margin-top: 80px;
+  margin-bottom: 50px;
+  font-size: 34px;
+
+  &::after {
+    content: '';
+    width: 100px;
+    height: 6px;
+    background-color: #66A2FE;
+    display: block;
+    margin: 10px auto 0 auto;
+  }
+`
+
+const Container = styled.div`
+  max-width: 900px;
+  width: 90%;
+  margin: auto;
+  @media(min-width: 992px){
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    column-gap: 2rem;
+  }
+`
+
+const Image = styled.img`
+  max-width: 400px;
+  width: 80%;
+  margin: 100px auto 0 auto;
+  display: block;
+`
