03cd6df78fe51fa8d3d5655e2dc05db00e322799
gracy94 <gracielafaria94@gmail.com>
2023-04-19 11:35:47 -0300

    static currencies name

---
+++ cryptocurrency-quote/src/data/currencies.js
@@ -0,0 +1,10 @@
+const currencies = [
+    { id: 'USD', name: 'United States Dollar'},
+    { id: 'ARS', name: 'Argentine pesos'},
+    { id: 'EUR', name: 'Euro'},
+    { id: 'BRL', name: 'Brazilian real'},
+]
+
+export {
+    currencies
+}
